5709ac28c5fc45f9767bee0bf9c263c6c1a7bea0
Saqib Bilal <saqib_bilal786@yahoo.com>
2025-07-04 23:36:28 -0400

    setting up unit tests for Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class task extends Model
+class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory;
